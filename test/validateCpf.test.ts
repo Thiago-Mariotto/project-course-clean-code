da4a28b67a7916045a1b0cab0e5f3627d234b6c1
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-09-25 20:49:13 -0300

    test: add cpf validation test

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,14 @@
+import { validateCpf } from "../src/cpf";
+
+test('Quando o número for valido, retorna true', function () {
+	const cpfIsValid = validateCpf('43545679896');
+	expect(cpfIsValid).toBeTruthy();
+});
+
+test('Quando o número for < 11 caracteres ou > 14 caracteres, retorna "Invalid CPF"', function () {
+	expect(() => validateCpf('1234567989')).toThrow(new Error('Invalid CPF'));
+});
+
+test('Quando o número for undefined/null retorna "Invalid CPF"', function () {
+	expect(() => validateCpf('1234567989')).toThrow(new Error('Invalid CPF'));
+});
