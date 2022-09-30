70dea5fbd86d3daa935dbc2b36917b976ad57483
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-09-29 22:28:50 -0300

    chore: implements order class

---
+++ src/Order.ts
@@ -0,0 +1,38 @@
+import Coupon from "./Coupon";
+import Cpf from "./Cpf";
+import Item from "./Item";
+import OrderItem from "./OrderItem";
+
+export default class Order extends Cpf {
+	private cpf: Cpf;
+	orderItems: OrderItem[];
+	coupon?: Coupon;
+
+	constructor(cpf: string) {
+		super(cpf);
+		this.cpf = new Cpf(cpf);
+		this.orderItems = [];
+	}
+
+	addItem(item: Item, quantity: number) {
+		this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+	}
+
+	addCoupon(coupon: Coupon) {
+		this.coupon = coupon;
+	}
+
+	getTotal() {
+		let total = this.orderItems.reduce((total, orderItem) => {
+			total += orderItem.getTotal();
+			return total;
+		}, 0);
+		if (this.coupon) {
+			const discount = this.coupon.getDiscount(total);
+			return total -= discount;
+		}
+
+		return total;
+	}
+}
+
