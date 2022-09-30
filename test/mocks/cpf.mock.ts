181aa858a8e7600caf638ad4ea50cbaa33860711
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-09-29 22:29:06 -0300

    test: mocks to test

---
+++ test/mocks/cpf.mock.ts
@@ -0,0 +1,5 @@
+export const validCpf = '19361862170';
+export const validCpfWithChars = '193.618.621-70'
+export const invalidCpf = '19361862456'
+export const invalidCpf2 = '19361862456323'
+export const incompleteCpf = '';
---
+++ test/mocks/order.mock.ts
@@ -0,0 +1,6 @@
+export const validOrder = {
+	cpf: '19361862170',
+	products: [1, 2, 3],
+	totalPrice: 10.59,
+	status: 'open'
+}
