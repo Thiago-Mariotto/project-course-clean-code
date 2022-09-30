acbcdd0c3048360a6e75c90759cd017b5c451dca
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-09-30 16:01:44 -0300

    test: add test date coupons

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -25,8 +25,38 @@ describe('Valida a criação de um novo pedido', function () {
 	test('deve criar um pedido com 3 itens e aplicar o cupom de desconto', function () {
 		const order = new Order('19361862170');
 		order.addItem(new Item(1, 'Monitor', 1000), 2);
-		order.addCoupon(new Coupon('10OFF', 10));
+		order.addCoupon(new Coupon('10OFF', 10, new Date()));
 		const total = order.getTotal();
 		expect(total).toBe(1800);
+	});
+
+	test('não deve criar um pedido com cupom expirado a 1 dia ou mais', function () {
+		 const order = new Order('19361862170');
+		order.addItem(new Item(1, 'Monitor', 1000), 2);
+		expect(() =>
+			order.addCoupon(new Coupon('10OFF', 10, new Date('01/01/2021'))))
+			.toThrow(new Error('Coupon expired'));
+	});
+
+	test('não deve criar um pedido com cupom com cupom expirado a 1 hora ou mais', function () {
+		const order = new Order('19361862170');
+		order.addItem(new Item(1, 'Monitor', 1000), 2)
+		var dateNow = new Date();
+		const oneHour = (1000 * 60 * 60);
+		var oneHourAgo = new Date(dateNow.getTime() - oneHour);
+		expect(() =>
+			order.addCoupon(new Coupon('10OFF', 10, new Date(oneHourAgo))))
+			.toThrow(new Error('Coupon expired'));
+	});
+
+	test('não deve criar um pedido com cupom expirado a 1 minuto ou mais', function () {
+		const order = new Order('19361862170');
+		order.addItem(new Item(1, 'Monitor', 1000), 2)
+		var dateNow = new Date();
+		const oneMinute = (1000 * 60 * 60);
+		var oneMinuteAgo = new Date(dateNow.getTime() - oneMinute);
+		expect(() =>
+			order.addCoupon(new Coupon('10OFF', 10, new Date(oneMinuteAgo))))
+			.toThrow(new Error('Coupon expired'));
 	});
 });
