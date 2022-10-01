29f0b5ea8dd50750dd92bee18f7b71e29cc54db8
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-01 11:15:52 -0300

    test: add item mocks

---
+++ test/mocks/item.mock.ts
@@ -0,0 +1,11 @@
+import Item from '../../src/Item';
+
+const monitor = new Item(1, 'Monitor', 1000, 17, 17, 22);
+const mouse = new Item(2, 'Mouse', 250, 17, 17, 21);
+const teclado = new Item(3, 'Teclado', 450, 17, 17, 21);
+
+export default {
+	monitor,
+	teclado,
+	mouse
+}
