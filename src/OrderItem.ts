4bbbc60f45918caf21933c2cc58821bf84197407
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-01 10:24:03 -0300

    chore: validate item order quantity

--- src/OrderItem.ts
+++ src/OrderItem.ts
@@ -1,6 +1,6 @@
 export default class OrderItem {
 	constructor(readonly idItem: number, readonly price: number, readonly quantity: number) {
-
+		if (this.quantity < 0) throw new Error('Invalid item quantity');
 	}
 
 	getTotal() {
