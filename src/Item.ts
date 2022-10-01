53c866a4411530d0724bc3b7c8d9f9dba508ebd4
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-01 11:26:11 -0300

    chore: add new attributes in Item about dimension

--- src/Item.ts
+++ src/Item.ts
@@ -1,5 +1,18 @@
-export default class Item {
-	constructor(readonly idItem: number, readonly description: string, readonly price: number) {
 
+export default class Item {
+	readonly idItem: number;
+	readonly description: string;
+	readonly price: number;
+	readonly height: number;
+	readonly width: number;
+	readonly depth: number;
+
+	constructor(idItem: number, description: string, price: number, height: number, width: number, depth: number) {
+		this.idItem = idItem;
+		this.description = description;
+		this.price = price;
+		this.depth = depth;
+		this.height = height;
+		this.width = width;
 	}
 }
