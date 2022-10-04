b8357e447f3c051d1d5f12add0574997d150b766
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-04 14:50:27 -0300

    test: add new tests to item

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -1,3 +1,5 @@
+import Dimension from '../src/domain/entity/Dimension';
+import Item from '../src/domain/entity/Item';
 import itemMock from './mocks/item.mock';
 
 describe('Valida a criação de um novo Item', function () {
@@ -7,6 +9,7 @@ describe('Valida a criação de um novo Item', function () {
 	});
 
 	test('Não é possível criar um item com dimensão inválida', function () {
-		expect(() => itemMock.invalidItemDimension).toThrow(new Error('Invalid item dimension'));
+		expect(() => new Item(1, 'iPhone 14', 6999, new Dimension(17, 17, -10, 0.4)))
+			.toThrow(new Error('Invalid dimensions'));
 	});
 });
