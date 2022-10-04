105617999921cf57a8f30569829a93eb2ac7384c
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-04 14:47:57 -0300

    chore : change files path to entity

--- src/domain/OrderItem.ts
+++
@@ -1,9 +0,0 @@
-export default class OrderItem {
-	constructor(readonly idItem: number, readonly price: number, readonly quantity: number) {
-		if (this.quantity < 0) throw new Error('Invalid item quantity');
-	}
-
-	getTotal() {
-		return this.price * this.quantity;
-	}
-}
