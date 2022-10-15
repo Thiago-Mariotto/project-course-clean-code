d3dadac73d1e916ca2c3c0836d3edfcf6e231b00
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-14 21:49:33 -0300

    test: remove mock files

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -2,7 +2,6 @@ import Coupon from "../src/domain/entity/Coupon";
 import Dimension from "../src/domain/entity/Dimension";
 import Item from "../src/domain/entity/Item";
 import Order from "../src/domain/entity/Order";
-import itemMock from './mocks/item.mock';
 
 describe('Valida a criação de um novo pedido', function () {
 	test('não deve criar um pedido com cpf inválido', function () {
@@ -11,11 +10,11 @@ describe('Valida a criação de um novo pedido', function () {
 
 	test('deve criar um pedido com 3 itens e calcular o total do pedido', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.mouse, 1);
-		order.addItem(itemMock.teclado, 1);
-		order.addItem(itemMock.monitor, 2);
+		order.addItem(new Item(1, 'Mouse', 100, new Dimension(10, 10, 10, 0.9)), 1);
+		order.addItem(new Item(2, 'Teclado', 300, new Dimension(10, 10, 10, 0.9)), 1);
+		order.addItem(new Item(3, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2);
 		const total = order.getTotal();
-		expect(total).toBe(2700);
+		expect(total).toBe(2600);
 	});
 
 	test('ao criar um pedido sem itens o total é 0', function () {
@@ -26,15 +25,15 @@ describe('Valida a criação de um novo pedido', function () {
 
 	test('deve criar um pedido com 3 itens e aplicar o cupom de desconto', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.monitor, 2);
+		order.addItem(new Item(3, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2);
 		order.addCoupon(new Coupon('10OFF', 10, new Date()));
 		const total = order.getTotal();
-		expect(total).toBe(1800);
+		expect(total).toBe(1980);
 	});
 
 	test('Não deve ser possível adicionar itens duplicados a um pedido', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.monitor, 2);
+		order.addItem(new Item(1, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2);
 		expect(() => order.addItem(new Item(1, 'Monitor', 1000, new Dimension(17, 17, 21, 1.2)), 1)).toThrow('Item has already been added');
 	});
 
@@ -45,7 +44,7 @@ describe('Valida a criação de um novo pedido', function () {
 
 	test('não deve criar um pedido com cupom expirado a 1 dia ou mais', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.monitor, 2);
+		order.addItem(new Item(3, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2);
 		expect(() =>
 			order.addCoupon(new Coupon('10OFF', 10, new Date('01/01/2021'))))
 			.toThrow(new Error('Coupon expired'));
@@ -53,7 +52,7 @@ describe('Valida a criação de um novo pedido', function () {
 
 	test('não deve criar um pedido com cupom com cupom expirado a 1 hora ou mais', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.monitor, 2)
+		order.addItem(new Item(3, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2)
 		var dateNow = new Date();
 		const oneHour = (1000 * 60 * 60);
 		var oneHourAgo = new Date(dateNow.getTime() - oneHour);
@@ -64,7 +63,7 @@ describe('Valida a criação de um novo pedido', function () {
 
 	test('não deve criar um pedido com cupom expirado a 1 minuto ou mais', function () {
 		const order = new Order('19361862170');
-		order.addItem(itemMock.monitor, 2)
+		order.addItem(new Item(3, 'Monitor', 1100, new Dimension(10, 10, 10, 0.9)), 2)
 		var dateNow = new Date();
 		const oneMinute = (1000 * 60 * 60);
 		var oneMinuteAgo = new Date(dateNow.getTime() - oneMinute);
